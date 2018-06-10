d92978c607dd0c9cc2f683db35dc2427569595cb
Noskcaj <kcajdev@gmail.com>
2018-06-10 10:01:43 -0400

    Add a script to find discord tokens

---
+++ find_token.py
@@ -0,0 +1,30 @@
+import subprocess
+import sqlite3
+
+
+def main():
+    output = subprocess.Popen(
+        ["find ~/ -name https*.discordapp.com_0.localstorage"],
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    results = output.communicate()[0].decode().splitlines()
+
+    print("Found:")
+    for i, result in enumerate(results, start=1):
+        print("{} - {}".format(i, result))
+
+    choice = input("Select a discord storage location: ")
+    choice = int(choice)
+    target = results[choice - 1]
+
+    conn = sqlite3.connect(target)
+    cursor = conn.cursor()
+    query = cursor.execute('SELECT value FROM  ItemTable WHERE key = "token"')
+    token = query.fetchone()[0].decode("utf-16-le")
+    print("Your discord token is:", token)
+
+
+if __name__ == "__main__":
+    main()
